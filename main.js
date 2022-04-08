771cb4b2dd0cbf106db6690496d98e5acb20a27a
Nizar Fergag <fergag99@gmail.com>
2022-04-08 16:11:18 +0200

    stampa una mail

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 var app = new Vue(
     {
-        el: 'root',
+        el: '#root',
         data: {
-
+            mail: ''
+        },
+        created(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then( (res) => {
+                console.log(res.data.response)
+                for(i=0; i<10; i++){
+                    this.mail = res.data.response
+                }
+                
+            })
         },
         methods: {
             
