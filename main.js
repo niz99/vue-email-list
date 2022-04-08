eabed6ca8f77aae4ac8444b6a9faf89d4a577850
Nizar Fergag <fergag99@gmail.com>
2022-04-08 16:21:13 +0200

    bonus eseguito - fine esercizio

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ var app = new Vue(
                 .then( (res) => {
                     console.log(res.data.response)
                     this.listaMail.push(res.data.response)
-
                 })
             }
             
